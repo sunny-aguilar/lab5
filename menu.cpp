34772a9b50006ac77a48dccb84cb72bd2fd20add
Sunny Aguilar <sunny.aguilar@gmail.com>
2019-02-08 15:25:15 -0800

    lab5

--- menu.cpp
+++ menu.cpp
@@ -26,7 +26,7 @@ void Menu::menuStart() {
 }
 
 /*********************************************************************
-** Description:     prompts user
+** Description:     prompts user to 
 *********************************************************************/
 string Menu::menuEnterString() {
     cout << " ENTER A STRING AND I WILL\n"
