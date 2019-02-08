526d7b0b6a218af5236063ac6070ce7a85914104
Sunny Aguilar <sunny.aguilar@gmail.com>
2019-02-08 15:27:33 -0800

    lab5

--- menu.cpp
+++ menu.cpp
@@ -26,7 +26,7 @@ void Menu::menuStart() {
 }
 
 /*********************************************************************
-** Description:     prompts user to enter a string to reverse
+** Description:     prompts user to enter a string to reverse. String
 *********************************************************************/
 string Menu::menuEnterString() {
     cout << " ENTER A STRING AND I WILL\n"
